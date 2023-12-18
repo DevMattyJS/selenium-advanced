573b1d6584e16d40dd57664a6125ecd79c82cf6d
DevMattyJS <matej.sliacky@protonmail.com>
2023-12-18 18:38:09 +0100

    Added ParameterTest class

--- src/test/java/tests/ParameterTest.java
+++ src/test/java/tests/ParameterTest.java
@@ -1,11 +1,10 @@
 package tests;
 
-import helpers.ExcelReader;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -13,16 +12,28 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.io.IOException;
 import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
 
-public class PrimeNumberTest {
+@RunWith(Parameterized.class)
+public class ParameterTest {
 
     private WebDriver driver;
-    private final String BASE_URL = "http://localhost/primenumber.php";
-    private final String TEST_DATA_PATH = "src/test/resources/data/data.xlsx";
-    private final String SHEET_NAME = "prime";
+    private int number;
+    private boolean expectedValue;
 
+    private String BASE_URL = "http://localhost/primenumber.php";
+
+    @Parameterized.Parameters
+    public static List<Object[]> getData() {
+        return Arrays.asList(new Object[][]{{1, true}, {2, true}, {3, true}, {4, false}});
+    }
+
+    public ParameterTest(int number, boolean expectedValue) {
+       this.number = number;
+       this.expectedValue = expectedValue;
+    }
 
     @Before
     public void setUp() {
@@ -32,26 +43,15 @@ public class PrimeNumberTest {
     }
 
     @Test
-    public void primeTest() throws IOException {
+    public void primeTestWithParameters() {
         WebElement numberInput = driver.findElement(By.xpath("//input[@type='number']"));
         WebElement primeButton = driver.findElement(By.xpath("//button[contains(@class, 'btn-danger')]"));
-        ExcelReader excelReader = new ExcelReader(TEST_DATA_PATH);
-        Sheet primeSheet = excelReader.getSheetByName(SHEET_NAME);
 
-        for (Row row : primeSheet) {
-            // skip the header of the table
-            if (row.getRowNum() == 0) {
-                continue;
-            }
+        numberInput.clear();
+        numberInput.sendKeys(String.valueOf(number));
+        primeButton.click();
 
-            int number = (int) row.getCell(0).getNumericCellValue();
-            boolean expectedValue = row.getCell(1).getBooleanCellValue();
-            numberInput.clear();
-            numberInput.sendKeys(String.valueOf(number));
-            primeButton.click();
-
-            checkResult(expectedValue);
-        }
+        checkResult(expectedValue);
     }
 
     @After
@@ -68,4 +68,5 @@ public class PrimeNumberTest {
                     .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='result text-center error']")));
         }
     }
+
 }
